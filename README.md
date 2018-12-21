1d3bcae1c9c1bb88f7bd9f7bb3244e75e4489e5f
xLeachimx <michael.huelsman@gmail.com>
2018-12-21 13:46:21 -0500

    building basic board state classes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OnitamAI
 An AI for winning the game Onitama
+
+
+###Requirements
+========
+<None>
---
+++ onitama/__init__.py
@@ -0,0 +1,7 @@
+# File: __init__.py
+# Package: onitama
+# Author: Michael Huelsman
+# Created On: 21 Dec 2018
+# Purpose:
+#   This is the __init__ file for the onitama package which defines
+#   classes for modeling games of onitama.
